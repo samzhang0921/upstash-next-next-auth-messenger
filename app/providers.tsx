b6ff16eef52dbcf93ccb720b347bcc5be893a053
samzhang0921 <Sam.Zhang@ynap.com>
2023-04-23 18:39:31 +0100

    fix missing props

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { SessionProvider } from "next-auth/react";
 
-export const Providers = ({children}: any)=>{
-    return <SessionProvider>{children}</SessionProvider>
+export const Providers = ({session, children}: any)=>{
+    return <SessionProvider session={session}>{children}</SessionProvider>
 }
